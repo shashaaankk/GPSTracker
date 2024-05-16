4c3c52357ae0e13dc95c175a68495f4f4f88cb36
amoghsgk <63579467+amoghsgk@users.noreply.github.com>
2024-05-16 13:56:01 +0200

    raw_gps val

--- app/src/main/java/com/example/gpstracker/MainActivity.java
+++ app/src/main/java/com/example/gpstracker/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.gpstracker;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView Display;
+    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +29,14 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Display = findViewById(R.id.display);
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                Display.setText(location.toString());
+            }
+        };
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,5000,1,locationListener);
     }
 }
